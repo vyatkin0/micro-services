ccb311ea6a949a4deb241e6642649e36438f9820
Aleksey Vyatkin <avyatkin@gmail.com>
2022-10-25 11:53:20 +0300

    Jwt authenticaion added to products service

--- products/src/products.ts
+++ products/src/products.ts
@@ -5,13 +5,15 @@ const PROTO_PATH = __dirname + '/products.proto';
 import * as grpc from '@grpc/grpc-js';
 import * as protoLoader from '@grpc/proto-loader';
 import * as sqlite3 from 'sqlite3';
+import * as jwt from 'jsonwebtoken';
+
 const sqlite = sqlite3.verbose();
 
 const packageDefinition = protoLoader.loadSync(
     PROTO_PATH,
     {
         keepCase: true,
-        longs: String,
+        longs: Number,
         enums: String,
         defaults: true,
         oneofs: true
@@ -24,28 +26,45 @@ interface ProductsGrpcObject {
 const products_proto = grpc.loadPackageDefinition(packageDefinition).products as grpc.GrpcObject|ProductsGrpcObject;
 
 const dbName = 'products.db';
+const jwtSignKey = '1234567890abcdef1234567890abcdef1234567890abcdef1234567890abcdef'; 
+const claimMsRole = 'http://schemas.microsoft.com/ws/2008/06/identity/claims/role';
+
+function authCall(call, callback, roles: string[] = []) {
+    const authorizations = call.metadata.get('authorization');
+    for (const auth of authorizations) {
+        const bearer = 'Bearer ';
+        if(auth.startsWith(bearer))
+        {
+            const decoded = jwt.verify(auth.substring(bearer.length), jwtSignKey, { audience: 'https://github.com/vyatkin0/micro-services', issuer: 'https://github.com/vyatkin0/micro-services' });
+            console.log(decoded);
+            if(decoded[claimMsRole] && decoded[claimMsRole].some(r=>['Admin', ...roles].includes(r)))
+            {
+                return decoded;
+            }
+
+            callback({
+                code: grpc.status.PERMISSION_DENIED,
+                message: 'Unauthorized',
+                });
+
+            throw new Error('Unauthorized');
+        }
+    }
+}
 
 function list(call, callback) {
     console.log('List');
 
-    const db = new sqlite3.Database(dbName, (err) => {
-        if (err) {
-            console.error(err.message);
-            return callback({
-                code: 400,
-                message: err.message,
-                status: grpc.status.INTERNAL
-              })
-        }
-    });
+    authCall(call, callback, ['User']);
+
+    const db = new sqlite.Database(dbName);
     
     db.all('SELECT id, name FROM products ORDER BY id', [], (err, rows) => {
         if (err) {
             console.error(err.message);
             return callback({
-                code: 400,
+                code: grpc.status.INTERNAL,
                 message: err.message,
-                status: grpc.status.INTERNAL
               })
         }
 
@@ -59,25 +78,16 @@ function list(call, callback) {
 
 function get(call, callback) {
     console.log('Get');
+    authCall(call, callback, ['User']);
 
-    const db = new sqlite3.Database(dbName, (err) => {
-        if (err) {
-            console.error(err.message);
-            return callback({
-                code: 400,
-                message: err.message,
-                status: grpc.status.INTERNAL
-              })
-        }
-    });
+    const db = new sqlite.Database(dbName);
 
     db.get('SELECT id, name FROM products WHERE id  = ?', [call.request.id], (err, row) => {
         if (err) {
             console.error(err.message);
             return callback({
-                code: 400,
+                code: grpc.status.INTERNAL,
                 message: err.message,
-                status: grpc.status.INTERNAL
               })
         }
 
@@ -99,35 +109,27 @@ function get(call, callback) {
 
 function create(call, callback) {
     console.log('Get');
+    authCall(call, callback);
     callback(null, { id: 1, name: 'Keyboard' });
 }
 
 function update(call, callback) {
     console.log('Get');
+    authCall(call, callback);
     callback(null, { id: 1, name: 'Keyboard' });
 }
 
 function deleteProduct(call, callback) {
     console.log('Delete');
-    const db = new sqlite3.Database(dbName, (err) => {
-        if(err)
-        {
-            console.error(err?.message);
-            return callback({
-                code: 400,
-                message: err?.message,
-                status: grpc.status.INTERNAL
-            });
-        }
-    });
+    authCall(call, callback);
+    const db = new sqlite.Database(dbName);
 
     db.get('SELECT id, name FROM products WHERE id  = ?', [call.request.id], (err, row) => {
         if (err) {
             console.error(err.message);
             return callback({
-                code: 400,
+                code: grpc.status.INTERNAL,
                 message: err.message,
-                status: grpc.status.INTERNAL
                 });
         }
 
@@ -143,9 +145,8 @@ function deleteProduct(call, callback) {
             if (err) {
                 console.error(err.message);
                 return callback({
-                    code: 400,
+                    code: grpc.status.INTERNAL,
                     message: err.message,
-                    status: grpc.status.INTERNAL
                     });
             }
 
@@ -162,9 +163,7 @@ function deleteProduct(call, callback) {
 }
 
 function seedSqlite() {
-    const db = new sqlite3.Database(dbName, (err) => {
-        console.error(err?.message);
-    });
+    const db = new sqlite.Database(dbName);
     
     db.serialize(() => {
         db.run('DROP TABLE IF EXISTS products')
