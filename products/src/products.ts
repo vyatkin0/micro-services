9228bfd4c82b48578f3db27128eb64d255020599
Aleksey Vyatkin <avyatkin@gmail.com>
2022-10-25 18:22:42 +0300

    Create and update handlers added

--- products/src/products.ts
+++ products/src/products.ts
@@ -1,13 +1,11 @@
-export { }
-
-const PROTO_PATH = __dirname + '/products.proto';
-
 import * as grpc from '@grpc/grpc-js';
 import * as protoLoader from '@grpc/proto-loader';
 import * as sqlite3 from 'sqlite3';
 import * as jwt from 'jsonwebtoken';
 
 const sqlite = sqlite3.verbose();
+
+const PROTO_PATH = __dirname + '/products.proto';
 
 const packageDefinition = protoLoader.loadSync(
     PROTO_PATH,
@@ -23,29 +21,27 @@ interface ProductsGrpcObject {
     Products: any
 }
 
-const products_proto = grpc.loadPackageDefinition(packageDefinition).products as grpc.GrpcObject|ProductsGrpcObject;
+const products_proto = grpc.loadPackageDefinition(packageDefinition).products as grpc.GrpcObject | ProductsGrpcObject;
 
 const dbName = 'products.db';
-const jwtSignKey = '1234567890abcdef1234567890abcdef1234567890abcdef1234567890abcdef'; 
+const jwtSignKey = '1234567890abcdef1234567890abcdef1234567890abcdef1234567890abcdef';
 const claimMsRole = 'http://schemas.microsoft.com/ws/2008/06/identity/claims/role';
 
 function authCall(call, callback, roles: string[] = []) {
     const authorizations = call.metadata.get('authorization');
     for (const auth of authorizations) {
         const bearer = 'Bearer ';
-        if(auth.startsWith(bearer))
-        {
+        if (auth.startsWith(bearer)) {
             const decoded = jwt.verify(auth.substring(bearer.length), jwtSignKey, { audience: 'https://github.com/vyatkin0/micro-services', issuer: 'https://github.com/vyatkin0/micro-services' });
             console.log(decoded);
-            if(decoded[claimMsRole] && decoded[claimMsRole].some(r=>['Admin', ...roles].includes(r)))
-            {
+            if (decoded[claimMsRole] && decoded[claimMsRole].some(r => ['Admin', ...roles].includes(r))) {
                 return decoded;
             }
 
             callback({
                 code: grpc.status.PERMISSION_DENIED,
                 message: 'Unauthorized',
-                });
+            });
 
             throw new Error('Unauthorized');
         }
@@ -58,14 +54,14 @@ function list(call, callback) {
     authCall(call, callback, ['User']);
 
     const db = new sqlite.Database(dbName);
-    
+
     db.all('SELECT id, name FROM products ORDER BY id', [], (err, rows) => {
         if (err) {
             console.error(err.message);
             return callback({
                 code: grpc.status.INTERNAL,
                 message: err.message,
-              })
+            })
         }
 
         callback(null, { products: rows });
@@ -88,35 +84,109 @@ function get(call, callback) {
             return callback({
                 code: grpc.status.INTERNAL,
                 message: err.message,
-              })
+            })
         }
 
-        if(!row?.id) {
+        if (!row?.id) {
             return callback({
-                code: 400,
                 message: `No product found with id ${call.request.id}`,
                 status: grpc.status.NOT_FOUND
-              })
+            })
         }
 
         callback(null, row);
     }).close((err) => {
         if (err) {
-        return console.error(err.message);
+            return console.error(err.message);
         }
     });
 }
 
 function create(call, callback) {
-    console.log('Get');
+    console.log('Create');
     authCall(call, callback);
-    callback(null, { id: 1, name: 'Keyboard' });
+
+    const db = new sqlite.Database(dbName);
+
+    db.run('INSERT INTO products(name) VALUES (?)', [call.request.name], function (err) {
+        if (err) {
+            console.error(err.message);
+            return callback({
+                code: grpc.status.INTERNAL,
+                message: err.message,
+            });
+        }
+        console.log(`${this.changes} products have been created`);
+
+        db.get('SELECT id, name FROM products WHERE id=?', [this.lastID], function (err, row) {
+            if (err) {
+                console.error(err.message);
+                return callback({
+                    code: grpc.status.INTERNAL,
+                    message: err.message,
+                });
+            }
+
+            if (!row?.id) {
+                return callback({
+                    message: `No product found with id ${call.request.id}`,
+                    status: grpc.status.NOT_FOUND
+                })
+            }
+
+            callback(null, row);
+        });
+    });
+
+    db.close((err) => {
+        if (err) {
+            return console.error(err.message);
+        }
+    });
 }
 
 function update(call, callback) {
-    console.log('Get');
+    console.log('Update');
     authCall(call, callback);
-    callback(null, { id: 1, name: 'Keyboard' });
+
+    const db = new sqlite.Database(dbName);
+
+    db.serialize(() => {
+        db.run('UPDATE products SET name=? WHERE id=?', [call.request.name, call.request.id], function (err) {
+            if (err) {
+                console.error(err.message);
+                return callback({
+                    code: grpc.status.INTERNAL,
+                    message: err.message,
+                });
+            }
+            console.log(`${this.changes} products have been updated`);
+        })
+            .get('SELECT id, name FROM products WHERE id=?', [call.request.id], function (err, row) {
+                if (err) {
+                    console.error(err.message);
+                    return callback({
+                        code: grpc.status.INTERNAL,
+                        message: err.message,
+                    });
+                }
+
+                if (!row?.id) {
+                    return callback({
+                        message: `No product found with id ${call.request.id}`,
+                        status: grpc.status.NOT_FOUND
+                    })
+                }
+
+                callback(null, row);
+            });
+    });
+
+    db.close((err) => {
+        if (err) {
+            return console.error(err.message);
+        }
+    });
 }
 
 function deleteProduct(call, callback) {
@@ -130,15 +200,14 @@ function deleteProduct(call, callback) {
             return callback({
                 code: grpc.status.INTERNAL,
                 message: err.message,
-                });
+            });
         }
 
-        if(!row?.id) {
+        if (!row?.id) {
             return callback({
-                code: 400,
                 message: `No product found with id ${call.request.id}`,
                 status: grpc.status.NOT_FOUND
-              })
+            })
         }
 
         db.run('DELETE FROM products WHERE id = ?', [row.id], function (err) {
@@ -147,7 +216,7 @@ function deleteProduct(call, callback) {
                 return callback({
                     code: grpc.status.INTERNAL,
                     message: err.message,
-                    });
+                });
             }
 
             callback(null, row);
@@ -164,21 +233,21 @@ function deleteProduct(call, callback) {
 
 function seedSqlite() {
     const db = new sqlite.Database(dbName);
-    
+
     db.serialize(() => {
         db.run('DROP TABLE IF EXISTS products')
-        .run('CREATE TABLE products(id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, name TEXT)')
-        .run('INSERT INTO products(name) VALUES (?), (?), (?)', ['Mouse', 'Keyboard', 'Display'], function (err) {
-            if (err){
-                throw err;
-            }
-            console.log(`Products have been inserted, last rowid is ${this.lastID}`);
-        });
+            .run('CREATE TABLE products(id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, name TEXT)')
+            .run('INSERT INTO products(name) VALUES (?), (?), (?), (?), (?)', ['Mouse', 'Keyboard', 'Display', 'Notebook', 'Phone'], function (err) {
+                if (err) {
+                    throw err;
+                }
+                console.log(`Products have been inserted, last rowid is ${this.lastID}`);
+            });
     });
-    
+
     db.close((err) => {
         if (err) {
-        return console.error(err.message);
+            return console.error(err.message);
         }
     });
 }
